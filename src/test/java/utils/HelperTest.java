8607d5f481c8f1c4701f4c13332fb1d1a4cdd11f
elzalatypova <latypova.elzaa@yandex.ru>
2021-05-14 23:22:39 +0500

    Добавление теста для проверки регионов и коэффициентов

---
+++ src/test/java/utils/HelperTest.java
@@ -0,0 +1,22 @@
+package core.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import core.settings.SettingsManager;
+import core.utils.Helper;
+
+public class HelperTest {
+
+	@Test
+	public void getRegionCoeffTest() {
+		
+		SettingsManager settings = new SettingsManager();
+		settings.KalininskyCoeff.setValue(1.5);
+		
+		double value = Helper.getRegionCoeff(settings.KalininskyCoeff.getName(), settings);
+		assertTrue(value == 1.5);
+	}
+	
+}

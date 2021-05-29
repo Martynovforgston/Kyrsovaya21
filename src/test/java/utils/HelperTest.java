49dc907e67510ff60714cfe4eb5449ec3f161fd0
elzalatypova <79845168+elzalatypova@users.noreply.github.com>
2021-05-29 17:57:14 +0500

    Изменение теста HelperTest

--- src/test/java/utils/HelperTest.java
+++ src/test/java/utils/HelperTest.java
@@ -4,19 +4,15 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import core.settings.SettingsManager;
 import core.utils.Helper;
 
 public class HelperTest {
 
 	@Test
-	public void getRegionCoeffTest() {
+	public void getRegionNameTest() {
 		
-		SettingsManager settings = new SettingsManager();
-		settings.KalininskyCoeff.setValue(1.5);
-		
-		double value = Helper.getRegionCoeff(settings.KalininskyCoeff.getName(), settings);
-		assertTrue(value == 1.5);
+		String regionName = Helper.getRegionName("Kalininsky");
+		assertTrue(regionName == "Калининский");
 	}
 	
 }
